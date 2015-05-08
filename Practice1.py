42b6171f80e76699cb6601e5ed776bafe02d7d51
jowon <18jbang@iaa.edu.ec>
2015-05-07 22:14:19 -0500

    Create Practice1.py

---
+++ Practice1.py
@@ -0,0 +1,14 @@
+name = "Jo Won" #string
+lastname = "Bang" 
+address = "Laguna Club" 
+email = "18jbang@iaa.edu.ec"
+PhoneNumber = "0246859" 
+
+print name
+print lastname
+print address
+print email
+print PhoneNumber
+
+print 'My name is', name, '. My last name is',lastname,
+print '. My address is', address, '. My email is', email, '. My phone number is', PhoneNumber
