ff181726749a476f95c550746008112377964cf2
jowon <18jbang@iaa.edu.ec>
2015-05-07 22:08:19 -0500

    Create HelloWorld.py

---
+++ HelloWorld.py
@@ -0,0 +1,21 @@
+print "Hello World" 
+print "My name is Jowon" 
+print "I am 14 years old \nThis is Phython" 
+
+print 2015 + 1 #Printing an integer
+print "2015"
+
+print 3.1416 #Printing a floating point number
+
+#Math Operations
+print 1+1
+print 1-1
+print 1*1
+print 1/1
+
+#Modulus
+print 10%3
+
+print *Odd or Even Numbers" 
+print 17%2
+print 16%2
