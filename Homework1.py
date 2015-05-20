21c20a4537d9f9d51eded6a9960acd6a167e4956
jowon <18jbang@iaa.edu.ec>
2015-05-20 09:51:09 -0500

    Create Homework1.py

---
+++ Homework1.py
@@ -0,0 +1,9 @@
+#Homework sys
+
+import sys
+
+print "Your name is: ", sys.argv[1]
+print "Your last name is: ", sys.argv[2]
+print "I was born in: ", sys.argv[4]
+print "I graduate in: ", sys.argv[5]
+print "My gpa is: ", sys.argv[6]
