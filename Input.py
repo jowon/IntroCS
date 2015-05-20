a3a1e0ced8bc0ccc875adf2034f447d8d503a328
jowon <18jbang@iaa.edu.ec>
2015-05-20 16:38:30 -0500

    Create Input.py

---
+++ Input.py
@@ -0,0 +1,11 @@
+name = raw_input("What is your name? ") 
+last = raw_input("What is your last name? ") 
+
+print "Your name is", name, last 
+
+age = input ("How old are you? ")
+print "You are", age, "yeard old" 
+print "In ten years you will be", age+10
+
+print age * 7
+print name * 7 
