5d7473505c6ad5c4325bfa27e00209c5dbf1556e
jowon <18jbang@iaa.edu.ec>
2015-05-19 22:07:17 -0500

    Create CommandLine.py

---
+++ CommandLine.py
@@ -0,0 +1,19 @@
+#name = "Jowon"
+#print "My name is", name
+
+import sys
+print "Your name is: ", sys.argv[1]
+print "Your middle name is: ", sys.argv[2]
+print "Your last name is: ", sys.argv[3]
+
+print "Your name is: ", sys.argv[1]
+print "Your last name is: ", sys.argv[2]
+print "I was born in: ", sys.argv[4]
+print "I graduate in: ", sys.argv[5]
+print "My gpa is: ", sys.argv[6]
+
+#Print First name
+#Print last name
+# print your year off birth
+#print your year of graduation
+#print your GPA
