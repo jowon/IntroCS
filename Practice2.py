b4db1e1d2962711f8dd9570ef0afe0f863ffdfa0
jowon <18jbang@iaa.edu.ec>
2015-05-20 18:25:14 -0500

    Create Practice2.py

---
+++ Practice2.py
@@ -0,0 +1,28 @@
+name = raw_input("yout name is?")
+last = raw_input("your last name is?")
+birth = input("when where you born?")
+singer = raw_input("what is your favorite band?")
+movie = raw_input("what is your favorite movie?")
+bff = raw_input("who is your best friend?")
+
+
+print "my name is", name
+print "my last name is", last 
+print "I was born in", birth
+print "my favorite singer is", singer
+print "my favorite movie is", movie 
+print "my best friend is", bff 
+print "I am currently", 2015-birth, "years old"
+
+print name*10
+
+#Create a form using input and raw_input
+#Name
+#Last Name
+#Year of Birth
+#Favorite singer / band
+#Favorite movie 
+#Best friend
+#Calculate current age 
+#Print everything in a string concentrating variables
+#Print the name 10 times
