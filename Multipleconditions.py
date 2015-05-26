d568536e1297c034e8632cb16908394cfd9d43f5
jowon <18jbang@iaa.edu.ec>
2015-05-26 10:31:06 -0500

    Create Multipleconditions.py

---
+++ Multipleconditions.py
@@ -0,0 +1,12 @@
+grade = input("Please enter your grade in numbers:") 
+
+if grade >= 90:
+    print "Your grade is: A"
+elif grade >= 80:
+    print "Your grade is: B"
+elif grade >=70:
+    print "Your grade is: C"
+elif grade >=60
+    print "Your grade is: D"
+else: 
+    print "Your grade is F" 
