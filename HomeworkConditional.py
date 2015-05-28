9d66b2946b765e4f5130d4288c3711ba5d5b7716
jowon <18jbang@iaa.edu.ec>
2015-05-28 09:43:24 -0500

    Create HomeworkConditional.py

---
+++ HomeworkConditional.py
@@ -0,0 +1,14 @@
+number = input("Please enter a number from 1 to 10:") 
+
+if number == 2:
+    print "The number is even"
+elif number == 4:
+    print "The number is even"
+elif number == 6:
+    print "The number is even"
+elif number == 8:
+    print "The number is even"
+elif number == 10:
+    print "The number is even"
+else:
+    print "The number is odd"
